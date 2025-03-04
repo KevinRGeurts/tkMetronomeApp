13abd0fdfac0141466ebd589277d21802c2813d0
krgeurts <krgeurts@icloud.com>
2025-03-04 10:00:07 -0600

    Added unit tests of ObserverPatternBase Subject and Observer classes

---
+++ test_ObserverPatternBase.py
@@ -0,0 +1,29 @@
+# Standard imports
+import unittest
+
+# Local imports
+from ObserverPatternBase import Subject, Observer
+
+class Test_Subject(unittest.TestCase):
+    def test_attach_notify_detach(self):
+        obs = Observer()
+        sub = Subject()
+        sub.attach(obs)
+        self.assertTrue(sub._observers.index(obs)>=0)
+        self.assertRaises(NotImplementedError, sub.notify)
+        sub.detach(obs)
+        self.assertRaises(ValueError, sub._observers.index, obs)
+
+    def test_attach_nonobserver(self):
+        obs = Subject()
+        sub = Subject()
+        self.assertRaises(AssertionError, sub.attach, obs)
+
+    def test_detach_missing_observer(self):
+        obs = Observer()
+        sub = Subject()
+        self.assertRaises(ValueError, sub.detach, obs)
+
+
+if __name__ == '__main__':
+    unittest.main()
